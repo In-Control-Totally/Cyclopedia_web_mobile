e1f57aca55e25b7c6095225ffdc2b22f894f62ce
Gavin Jamieson <s3925654@student.rmit.edu.au>
2022-10-06 17:07:17 +1100

    User feedback implemented to journey page.

--- journey/db.js
+++ journey/db.js
@@ -10,20 +10,21 @@ window.addEventListener('load', (event) => {
     document.getElementById('idStartJourneyButton').addEventListener('click', getPosition);
     document.getElementById('idStartJourneyButton').addEventListener('click', startCapture);
     getLocation();
-    
+
 });
 
 
 
 function startCapture() {
     document.getElementById('idStartJourneyButton').setAttribute("disabled", "")
+    document.getElementById('idStatusNotificationArea').appendChild(createNotification('Running', 'bg-info'))
 }
 
 
 
 function handleAbort() {
     abortController.abort();
-    
+
     var transmission = {
         "journey": {
             "user_id": 1,
@@ -33,9 +34,40 @@ function handleAbort() {
         "points": stored_gps_data
     };
     console.log(transmission);
-    
-    postData('https://api-dev.cyclopedia.goldenrivet.xyz/journey/create', transmission)
-    //postData('http://127.0.0.1:8000/journey/create', transmission)
+    document.getElementById('notfication').remove();
+    document.getElementById('idStatusNotificationArea').appendChild(createNotification('Submitting', 'bg-warning'));
+
+    postData('http://127.0.0.1:8000/journey/create', transmission)
+        //    postData('https://api-dev.cyclopedia.goldenrivet.xyz/journey/create', transmission)
+        .then((response) => {
+
+                stored_gps_data = [];
+                if (response.status == 200) {
+                    document.getElementById('notfication').remove();
+                    document.getElementById('idStatusNotificationArea').appendChild(createNotification('Submitted', 'bg-success'));
+                } else {
+                    document.getElementById('notfication').remove();
+                    document.getElementById('idStatusNotificationArea').appendChild(createNotification('Failed - ' + response.status, 'bg-danger'));
+
+                }
+            }
+
+        )
+        .catch((err) => {
+            document.getElementById('notfication').remove();
+            document.getElementById('idStatusNotificationArea').appendChild(createNotification('Failed - Check console', 'bg-danger'));
+            console.log(err);
+        })
+
+}
+
+function createNotification(note_text, type_of_badge) {
+    var new_element = document.createElement("span");
+    new_element.classList.add("badge");
+    new_element.classList.add(type_of_badge);
+    new_element.setAttribute("id", "notfication");
+    new_element.innerHTML = note_text;
+    return new_element
 }
 
 
@@ -43,16 +75,15 @@ function handleAbort() {
 function getPosition() {
 
     let runner = setTimeout(() => {
-            getPosition2()
-        }
-        , 1000);
+        getPosition2()
+    }, 1000);
 }
 
 
 function getPosition2() {
     if (!abortSignal.aborted) {
         navigator.geolocation.getCurrentPosition((position) => {
-                    console.log(stored_gps_data);
+
             stored_gps_data.push({
                 "latitude": String(position.coords.latitude),
                 "longitude": String(position.coords.longitude),
